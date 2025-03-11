0751ddcb31e0170d564e7d1f9ccec0ca8c03250c
ShashikantYadav0111 <yadavshashikant43@gmmail.com>
2025-03-12 01:30:28 +0530

    imporved UI

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { MainComponent } from './components/main/main.component';
+import { FeedbackComponent } from './pages/feedback/feedback.component';
+import { SupportComponent } from './pages/support/support.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:'',redirectTo:'home',pathMatch:'full'},
+    {path:'home',component:MainComponent},
+    {path:'feedback',component:FeedbackComponent},
+    {path:'support',component:SupportComponent},
+    
+
+];
